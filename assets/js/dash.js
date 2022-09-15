535842bc60669265adc25357f4276fca35e72acc
FolorunshoAyo <folushoayomide11@gmail.com>
2022-09-15 01:08:09 +0100

    Created the admin side-menu

--- assets/js/dash.js
+++ assets/js/dash.js
@@ -1,21 +1,35 @@
 $(function(){
-    const mobileMenuIcon = $(".menu-icon-container");
-    const mobileMenu = $("aside.mobile-menu");
-    const mobileBackdrop = $(".mobile-backdrop");
-    const closeMenuEl = $(".cross-icon-container");
-
-    mobileMenuIcon.on("click", function(){
-        mobileMenu.addClass("active");
-        mobileBackdrop.addClass("active");
+    const aside = $(".dash-menu");
+    const icon = $(".menu-icon");
+    const li = $(".nav-item");
+    const mobileBackdrop =$(".mobile-backdrop");
+    const dropdownBtn = $(".arrow-container");
+    const dropdownMenu = $(".dropdown-menu");
+    
+    // EXPAND DASH MENU FUNCTIONALITY ON BUTTON CLICK
+    icon.on("click", function(){
+        aside.toggleClass("expand");
+        mobileBackdrop.toggleClass("display");
+    });
+    
+    // NAVIGATION LINK DROPDOWN FUNCTIONALITY START
+    new MetisMenu('#side-menu',{
+        // selector of parent trigger
+        parentTrigger: "li",
+        // selector of submenu
+        subMenu: "ul",
+        // enable auto collapse
+        toggle: true,
+        // trigger element
+        triggerElement: "a"
+        
     });
 
-    closeMenuEl.on("click", function(){
-        mobileMenu.removeClass("active");
-        mobileBackdrop.removeClass("active");
-    });
 
-    mobileBackdrop.on("click", function(){
-        mobileMenu.removeClass("active");
-        mobileBackdrop.removeClass("active");
-    })
+    // DROPDOWN BUTTON FUNCTONALITY 
+    dropdownBtn.on("click", function() {
+        dropdownBtn.toggleClass("active");
+        dropdownMenu.toggleClass("active");
+    });
+       
 });
